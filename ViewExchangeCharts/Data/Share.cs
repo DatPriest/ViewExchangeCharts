64871efa59448451e799268d66caad783ff06c83
DatPriest <43251311+DatPriest@users.noreply.github.com>
2022-03-13 20:17:35 +0100

    Added first Page and first Data initialization

---
+++ ViewExchangeCharts/Data/Share.cs
@@ -0,0 +1,15 @@
+﻿namespace ViewExchangeCharts.Data
+{
+    public class Share
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public TimeSeriesData Data { get; set; }
+    }
+
+    public struct TimeSeriesData
+    {
+        public DateTime Date;
+        public string Close;
+    }
+}
---
+++ ViewExchangeCharts/Data/ShareService.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ViewExchangeCharts.Data
+{
+    public class ShareService
+    {
+        public List<TimeSeriesData> GetShare(string Symbol = "predefined")
+        {
+            if (string.IsNullOrWhiteSpace(Symbol))
+            {
+                throw new ArgumentException($"\"{nameof(Symbol)}\" darf nicht NULL oder ein Leerraumzeichen sein.", nameof(Symbol));
+            }
+
+
+
+            var exampleData = "";
+
+            return ReadJsonToShare(); ;
+        }
+
+        private List<TimeSeriesData> ReadJsonToShare(string json = "")
+        {
+            using StreamReader file = File.OpenText(@"X:\Dev\Visual Studio Projekte\ViewExchangeCharts\ViewExchangeCharts\Data\exampleShareTimeSeriesData.json");
+            using JsonTextReader reader = new(file);
+            JObject? obj = JToken.ReadFrom(reader)?["Time Series (Daily)"] as JObject;
+            List<TimeSeriesData> list = new();
+            foreach (var share in obj)
+            {
+                TimeSeriesData data = new TimeSeriesData
+                {
+                    Date = DateTime.Parse(share.Key),
+                    Close = share.Value["4. close"].ToString()
+                };
+                list.Add(data);
+            }
+            return list;
+
+        }
+    }
+}
